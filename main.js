e2a70e8aa03b61928772f22da86db05157a1216a
harry walker <harrywalker312@gmail.com>
2019-09-24 08:43:15 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+  function setNewImage() {
+      document.getElementById("img1").src = "assets/images/watch-front-copy.png";
+  }
+  
+  function setOldImage() {
+    document.getElementById("img1").src = "assets/images/watch.png";
+}
+
+
+/* removes class makeRed, adds class makeBorder on mouseenter
+$("button").mouseenter(function(){
+    $(this).removeClass("makeRed").addClass("makeBorder");
+    });
+    
+    $("button").mouseleave(function(){
+    $("button").removeClass("makeBorder").addClass("makeRed");
+    }); */
